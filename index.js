8b9e144d50eb9127b1b677c10f387eea0bd4f157
aydev <aayush.kumar@famepilot.com>
2020-07-10 07:43:43 +0530

    Port Config

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   	res.sendFile(__dirname + '/index.html');
 });
@@ -20,6 +22,6 @@ io.on('connection', (socket) => {
 
 });
 
-http.listen(3000, () => {
+http.listen(PORT, () => {
   	console.log('listening on *:3000');
 });
